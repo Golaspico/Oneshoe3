d139fa097cfb8b699961c9d51ee538a63feace1a
dynazero <garcia.jimmyj@gmail.com>
2016-09-08 00:22:26 +0800

    merged

---
+++ oneshoe.sql
@@ -0,0 +1,178 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 07, 2016 at 12:24 AM
+-- Server version: 10.1.13-MariaDB
+-- PHP Version: 5.6.21
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `oneshoe`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `carts`
+--
+
+CREATE TABLE `carts` (
+  `cartsID` int(11) NOT NULL,
+  `UsersID` int(11) NOT NULL,
+  `ProductsID` int(11) NOT NULL,
+  `SizesID` int(11) NOT NULL,
+  `Amount` int(11) NOT NULL,
+  `Price` int(11) NOT NULL,
+  `TotalAmount` int(11) NOT NULL,
+  `OrderID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `OrderID` int(11) NOT NULL,
+  `UsersID` int(11) NOT NULL,
+  `Status` int(11) NOT NULL,
+  `Message` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE `products` (
+  `ProductsID` int(11) NOT NULL,
+  `UsersID` int(11) NOT NULL,
+  `ProductName` text NOT NULL,
+  `ProductPrice` float(11,0) NOT NULL,
+  `Image` text NOT NULL,
+  `Category` int(11) NOT NULL DEFAULT '1',
+  `Details` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`ProductsID`, `UsersID`, `ProductName`, `ProductPrice`, `Image`, `Category`, `Details`) VALUES
+(7, 1, 'menchook', 321, 'mizuno-rockman-exclusive-truforce-kickstarter-4-400x250 (1).jpg', 3, 'bbfasd'),
+(8, 1, 'asdfasd', 321, 'AizensWallet2.PNG', 1, 'adasf');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sizes`
+--
+
+CREATE TABLE `sizes` (
+  `SizesID` int(11) NOT NULL,
+  `ProductsID` int(11) NOT NULL,
+  `Size` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `UsersID` int(11) NOT NULL,
+  `UserName` text NOT NULL,
+  `Password` text NOT NULL,
+  `FullName` text NOT NULL,
+  `Address` text NOT NULL,
+  `Email` text NOT NULL,
+  `Role` int(11) DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`UsersID`, `UserName`, `Password`, `FullName`, `Address`, `Email`, `Role`) VALUES
+(1, 'John', '0cef1fb10f60529028a71f58e54ed07b', 'John Smith', '123 Heaven st. Devil City', 'Hwoarang@yahoo.com', 0),
+(4, 'Michael', '0b9f827b8f5033c864bc7f21d334db20', 'Michael Jordan', '123 Magic City of Ohio', 'MJ@yahoo.com', 0);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `carts`
+--
+ALTER TABLE `carts`
+  ADD PRIMARY KEY (`cartsID`);
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`OrderID`);
+
+--
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`ProductsID`);
+
+--
+-- Indexes for table `sizes`
+--
+ALTER TABLE `sizes`
+  ADD PRIMARY KEY (`SizesID`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`UsersID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `carts`
+--
+ALTER TABLE `carts`
+  MODIFY `cartsID` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `OrderID` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `ProductsID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+--
+-- AUTO_INCREMENT for table `sizes`
+--
+ALTER TABLE `sizes`
+  MODIFY `SizesID` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `UsersID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
